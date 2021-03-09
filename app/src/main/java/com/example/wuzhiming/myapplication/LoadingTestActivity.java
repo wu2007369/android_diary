bf58b4d9379026c157d92a9a5227fb79412ce384
wuzhiming <wu2007369@126.com>
2021-03-09 16:23:34 +0800

    dairy

--- app/src/main/java/com/example/wuzhiming/myapplication/LoadingTestActivity.java
+++ app/src/main/java/com/example/wuzhiming/myapplication/LoadingTestActivity.java
@@ -3,6 +3,7 @@ package com.example.wuzhiming.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.github.ybq.android.spinkit.SpinKitView;
 import com.github.ybq.android.spinkit.SpriteFactory;
@@ -16,9 +17,14 @@ public class LoadingTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_test);
 
-        SpinKitView spin_kit=findViewById(R.id.spin_kit);
+/*        SpinKitView spin_kit=findViewById(R.id.spin_kit);
         Sprite drawable = SpriteFactory.create(Style.FADING_CIRCLE);
-        spin_kit.setIndeterminateDrawable(drawable);
+        spin_kit.setIndeterminateDrawable(drawable);*/
+
+        SpinKitView spin_kit=findViewById(R.id.spin_kit);
+        spin_kit.setVisibility(View.GONE);
+        spin_kit.setVisibility(View.VISIBLE);
+
 
     }
 }
--- app/src/main/java/com/example/wuzhiming/myapplication/intent/IntentActivity.java
+++ app/src/main/java/com/example/wuzhiming/myapplication/intent/IntentActivity.java
@@ -4,32 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.wuzhiming.myapplication.R;
 
 public class IntentActivity extends AppCompatActivity implements View.OnClickListener {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Uri locationForPhotos;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
+        image=findViewById(R.id.image);
 
         findViewById(R.id.btn14).setOnClickListener(this);
         findViewById(R.id.btn1).setOnClickListener(this);
         findViewById(R.id.btn2).setOnClickListener(this);
         findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn14:
-                Intent intent2=new Intent();
-                ComponentName cn = new ComponentName("com.jinying.mobile",  "com.jinying.mobile.wxapi.WXEntryActivity");
+                Intent intent2 = new Intent();
+                ComponentName cn = new ComponentName("com.jinying.mobile", "com.jinying.mobile.wxapi.WXEntryActivity");
 //                ComponentName cn = new ComponentName("net.sourceforge.simcpux",  "net.sourceforge.simcpux.wxapi.WXEntryActivity");
                 intent2.setComponent(cn);
                 startActivity(intent2);
@@ -37,27 +46,44 @@ public class IntentActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.btn1:
 //                startActivity(new Intent(this,AliasActivity.class));
-                startActivity(new Intent(this,JumpActivity.class));
+                startActivity(new Intent(this, JumpActivity.class));
                 break;
 
             case R.id.btn2:
 //                startActivity(new Intent(this,AliasActivity.class));
-                Intent intent=new Intent();
-                ComponentName cn2 = new ComponentName("com.jinying_oa",  "com.jinying_oa.activity.GuideActivity");
+                Intent intent = new Intent();
+                ComponentName cn2 = new ComponentName("com.jinying_oa", "com.jinying_oa.activity.GuideActivity");
                 intent.setComponent(cn2);
                 startActivity(intent);
                 break;
 
             case R.id.btn3:
 //                startActivity(new Intent(this,AliasActivity.class));
-                Intent intent3=new Intent();
+                Intent intent3 = new Intent();
                 intent3.setAction("android.intent.action.VIEW");
                 intent3.addCategory("android.intent.category.BROWSABLE");
                 intent3.setData(Uri.parse("geoaapp://"));
                 startActivity(intent3);
                 break;
+            case R.id.btn4:
+//                startActivity(new Intent(this,AliasActivity.class));
+                Intent intent4 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent4.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.withAppendedPath(locationForPhotos, "targetFilename"));
+                startActivityForResult(intent4, REQUEST_IMAGE_CAPTURE);
+                break;
             default:
                 break;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap thumbnail = data.getParcelableExtra("data");
+            Glide.with(this).load(thumbnail).into(image);
         }
     }
 }
